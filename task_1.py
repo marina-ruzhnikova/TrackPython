5385e9c9c9002bdeea4f2583ca44151d23dae451
marina-ruzhnikova <marina-ruzhka0306@yandex.ru>
2026-01-09 22:15:11 +0300

    Лабораторная работа №3

---
+++ task_1.py
@@ -0,0 +1,14 @@
+def search_items(list_, item):
+    if item in list_:
+        return list_.index(item)
+    else:
+        return None
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = search_items(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
